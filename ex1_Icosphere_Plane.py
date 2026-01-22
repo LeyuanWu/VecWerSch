4b71ab7034dbc95d9ff3054c1579e7a7e751d3a4
LeyuanWu <leyuanwu@zjut.edu.cn>
2026-01-22 19:43:02 +0800

    LW_20260122

--- ex1_Icosphere_Plane.py
+++ ex1_Icosphere_Plane.py
@@ -7,36 +7,50 @@ import pyvista as pv;
 import numpy as np;
 import pandas as pd;
 import time;
-from gravity_forward_numpy import gsphere;
-from gravity_forward_numba import VecWerSch_numba;
+from gravity_forward_numpy import *;
+from gravity_forward_numba import *;
 # %%
-# # ! Icosphere
-xc = 0.1; yc = 0.0; zc = 2.0;
+# # ! Icosphere <Geometry>
+xc, yc, zc = 0.1, 0.0, 2.0;
 a = 1.5;
-rho   = 1000.;
-xgv = np.linspace(-10., 10., 101);
-ygv = np.linspace(-10., 10., 101);
+vol = 4*np.pi*a**3/3; area = 4*np.pi*a**2;
+nsub_max = 10;
+Nsubs = np.arange(nsub_max);
+NVs = np.zeros(nsub_max, dtype=int);
+NFs = np.zeros(nsub_max, dtype=int);
+Es_vol = np.zeros(nsub_max);
+Es_area = np.zeros(nsub_max);
+MINs_psi = np.zeros(nsub_max);
+MAXs_psi = np.zeros(nsub_max);
+for insub, nsub in enumerate(Nsubs):
+    icosph = pv.Icosphere(radius=a, center=(xc, yc, zc), nsub=nsub);
+    Vert  = icosph.points;
+    Faces = icosph.regular_faces;
+    NVs[insub] = Vert.shape[0];
+    NFs[insub] = Faces.shape[0];
+    Es_vol[insub] = 1. - icosph.volume/vol;
+    Es_area[insub] = 1. - icosph.area/area;
+    vert_unit = (Vert - np.array([xc, yc, zc]))/a;
+    min_psi, max_psi = spherical_edge_length_range(vert_unit, Faces);
+    MINs_psi[insub] = 60. * np.rad2deg(min_psi); # arc-min
+    MAXs_psi[insub] = 60. * np.rad2deg(max_psi);
+print(f"{'nsub':>4} {'N_vertices':>10} {'N_faces':>10} "
+      f"{'Vol_err':>10} {'Area_err':>10} {'Min_psi':>10} {'Max_psi':>10}")
+print("-" * 63)
+for i in range(len(Nsubs)):
+    print(f"{Nsubs[i]:>4} {int(NVs[i]):>10} {int(NFs[i]):>10} "
+          f"{Es_vol[i]:>10.2e} {Es_area[i]:>10.2e} "
+          f"{MINs_psi[i]:>10.2f} {MAXs_psi[i]:>10.2f}")
+# %%
+# # ! Icosphere <Gravity>
+rho = 1000.;
+xgv = np.linspace(-10., 10., 51);
+ygv = np.linspace(-10., 10., 51);
 z0 = 0.;
 [X2d, Y2d] = np.meshgrid(xgv, ygv);
 Z2d = z0 * np.ones(X2d.shape);
 XPs, YPs, ZPs = map(lambda x: x.flatten(), [X2d, Y2d, Z2d]);
 P = np.column_stack((XPs, YPs, ZPs));
-
-vol = 4*np.pi*a**3/3; area = 4*np.pi*a**2;
-print(f'Sphere volume: {vol:12.6f}', flush=True);
-print(f'Sphere area: {area:12.6f}', flush=True);
-Ns = np.arange(9);
-for N in Ns:
-    icosph = pv.Icosphere(radius=a, center=(xc, yc, zc), nsub=N);
-    Vert  = icosph.points;
-    Faces = icosph.regular_faces;
-    print(f'N : {N}');
-    print(f'Number of <Vertex> : {Vert.shape[0]}');
-    print(f'Number of <Faces>  : {Faces.shape[0]}');
-    re_vol = np.abs(icosph.volume-vol)/vol * 100;
-    re_area = np.abs(icosph.area-area)/area * 100;
-    print(f'Icosphere volume: {icosph.volume:12.6f}, e={re_vol:.4f}%', flush=True);
-    print(f'Icosphere area: {icosph.area:12.6f}, e={re_area:.4f}%', flush=True);
 ######## * Sphere
 t1 = time.time();
 V, gx, gy, gz, Txx, Tyy, Tzz, Txy, Txz, Tyz \
@@ -72,7 +86,6 @@ def print_table(df, title):
 print_table(df_ref, "Reference")
 print_table(df_cal, "Polyhedron")
 print_table(df_diff, "Difference")
-
 # %%
 df_ref
 # %%
