371ad79faccfccfa0bea405ba353bad61ec7588e
LeyuanWu <leyuanwu@zjut.edu.cn>
2026-03-26 21:12:06 +0800

    LW_20260326

--- ex_Moon_TopoGrav_Comp_PH.py
+++ ex_Moon_TopoGrav_Comp_PH.py
@@ -7,112 +7,96 @@ import pyshtools as pysh
 import pygmt
 import pyvista as pv
 import time
+from gravity_forward_numba import *
 # %% 
 # # ! Constants
+myG = 6.67430e-11
 G = pysh.constants.G.value
 gm_moon = pysh.constants.Moon.gm.value
-r_calc = 1748000.0
-r_ref = 1738000.0
+r_calc = 1748000.0 / 1.e3
 # %% 
-# # ! Shape/Topo of Moon
+# # ! Shape of Moon
 #### * Shape
-clm_shp_moon = pysh.SHCoeffs.from_file('Moon_shape_2879.sh', 
-                                       lmax=2879, 
+lmax_shp = 359
+clm_shp_moon = pysh.SHCoeffs.from_file(f'Moon_shape_719.sh', 
+                                       lmax=lmax_shp, 
                                        name='LOLA_shape (Moon)',
                                        units='m', format='bshc')
-res_topo = 0.25
-lmax_pk = int(90/res_topo)-1
-grd_shp_moon = clm_shp_moon.expand(lmax=int(90/res_topo)-1,
-                                   lmax_calc=lmax_pk)
-grd_topo_moon = (grd_shp_moon - r_ref) / 1.e3
-# %% 
-# # ! Computation of topographic potential: Spectral-domain 
-nthreads = 1
-NMAX = 21
-print('nmax | time_sec |         min |         max |        mean |         std')
-print('-'*70)
-for nmax in range(1, NMAX):
-    t0 = time.time()
-    clm_topograv_moon = \
-        pysh.SHGravCoeffs.from_shape(shape=clm_shp_moon,
-                                     rho=2560.0,
-                                     gm=gm_moon,
-                                     nmax=nmax,
-                                     lmax=lmax_pk,
-                                     lmax_grid=lmax_pk,
-                                     lmax_calc=lmax_pk,
-                                     name=f'LOLA_Topo_Grav (Moon) nmax={nmax}',
-                                     backend='ducc',
-                                     nthreads=nthreads)
-    tc = time.time() - t0
-    res_topograv = res_topo
-    grd_grav_moon = clm_topograv_moon.expand(a=r_calc,
-                                             f=0.0,
-                                             lmax=int(90/res_topograv)-1, 
-                                             lmax_calc=lmax_pk)
-    gz_topo = -1.e5 * grd_grav_moon.rad
-    min_val = np.min(gz_topo.data)
-    max_val = np.max(gz_topo.data)
-    mean_val = np.mean(gz_topo.data)
-    std_val = np.std(gz_topo.data)
-    print(f"{nmax:4d} | {tc:8.3f} | {min_val:12.4f} | {max_val:12.4f} | {mean_val:12.4f} | {std_val:12.4f}")
-#### * Mapping
-fig = pygmt.Figure()
-gz_topo.plotgmt(fig=fig,
-                projection='mollweide',
-                central_longitude=-90.,
-                grid=[30, 30],
-                tick_interval=None,
-                cmap='vik',
-                cmap_limits=[-800, 800],
-                colorbar='bottom',
-                cb_triangles='both',
-                cb_label='Topographic radial gravity (mGal)',
-                cb_tick_interval=200,
-                cb_minor_tick_interval=100,
-                shading=grd_topo_moon)
-fig.show(width=800)
-######## * Stats
-Gs = {'Topographic gravity: radial (mGal)': gz_topo.to_array()}
-stats = {
-    name: {'Min': arr.min(), 'Max': arr.max(),
-           'Mean': arr.mean(), 'Std': arr.std()}
-    for name, arr in Gs.items()}
-df = pd.DataFrame(stats).T
-df.style.format("{:.4f}").set_properties(**{'text-align': 'center'})
+r_itfc = clm_shp_moon.coeffs[0,0,0] / 1.e3
+grd_shp_moon = clm_shp_moon.expand()
 # %% 
 # # ! Computation of topographic potential: Spatial-domain 
+rho0 = 2560.0 # kg/m^3
+#### * Polyhedron geometry
 mesh_shp_moon = pv.Sphere(radius=1.0, center=(0.0, 0.0, 0.0),
                           theta_resolution=grd_shp_moon.nlon-1,
                           phi_resolution=grd_shp_moon.nlat)
 [LON, LAT] = np.meshgrid(grd_shp_moon.lons(), grd_shp_moon.lats())
-RTOPO = grd_shp_moon.data
-X = RTOPO * np.cos(np.deg2rad(LAT)) * np.cos(np.deg2rad(LON))
-Y = RTOPO * np.cos(np.deg2rad(LAT)) * np.sin(np.deg2rad(LON))
-Z = RTOPO * np.sin(np.deg2rad(LAT))
-pts_x = X[1:-1,:-1].flatten(order='F')
-pts_y = Y[1:-1,:-1].flatten(order='F')
-pts_z = Z[1:-1,:-1].flatten(order='F')
-pts_topo = np.column_stack((pts_x, pts_y, pts_z))
-north_pole = np.array([X[0,0], Y[0,0], Z[0,0]]);
-south_pole = np.array([X[-1,0], Y[-1,0], Z[-1,0]]);
-Verts = np.insert(pts_topo, 0, np.vstack([north_pole, south_pole]), axis=0)
+R_SHP = grd_shp_moon.data/1.e3
+X = R_SHP * np.cos(np.deg2rad(LAT)) * np.cos(np.deg2rad(LON))
+Y = R_SHP * np.cos(np.deg2rad(LAT)) * np.sin(np.deg2rad(LON))
+Z = R_SHP * np.sin(np.deg2rad(LAT))
+# north pole, south pole, and regular points
+pts_x = np.hstack([X[0,0], X[-1,0], X[1:-1,:-1].flatten(order='F')]) 
+pts_y = np.hstack([Y[0,0], Y[-1,0], Y[1:-1,:-1].flatten(order='F')])
+pts_z = np.hstack([Z[0,0], Z[-1,0], Z[1:-1,:-1].flatten(order='F')])
+Verts = np.column_stack((pts_x, pts_y, pts_z))
 mesh_shp_moon.points = Verts
 Faces = mesh_shp_moon.regular_faces
-pts_r = np.sqrt(np.sum(Verts**2, axis=1))
+#### * Calculation points
+lon_p = np.arange(0, 361, 5)
+lat_p = np.arange(90, -91, -5)
+[LON_P, LAT_P] = np.meshgrid(lon_p, lat_p)
+X_calc = r_calc * np.cos(np.deg2rad(LAT_P)) * np.cos(np.deg2rad(LON_P))
+Y_calc = r_calc * np.cos(np.deg2rad(LAT_P)) * np.sin(np.deg2rad(LON_P))
+Z_calc = r_calc * np.sin(np.deg2rad(LAT_P))
+calc_lon_rad = np.deg2rad(LON_P.flatten())
+calc_lat_rad = np.deg2rad(LAT_P.flatten())
+calc_x = X_calc.flatten()
+calc_y = Y_calc.flatten()
+calc_z = Z_calc.flatten()
+P = np.column_stack((calc_x, calc_y, calc_z))
+t0 = time.time()
+V, gx, gy, gz, Txx, Txy, Txz, Tyy, Tyz, Tzz = WerSch_numba(P, Verts, Faces, rho0)
+t1 = time.time() - t0
+print(f"Time cost : {t1:8.3f} sec \n"
+      f"Number of points: {P.shape[0]} \n"
+      f"Number of faces: {Faces.shape[0]} \n")
+V0, gx0, gy0, gz0, Txx0, Txy0, Txz0, Tyy0, Tyz0, Tzz0 = \
+    gsphere(calc_x, calc_y, calc_z, 0, 0, 0, r_itfc, rho0)
+V = V - V0
+gx = gx - gx0
+gy = gy - gy0
+gz = gz - gz0
+Txx = Txx - Txx0
+Txy = Txy - Txy0
+Txz = Txz - Txz0
+Tyy = Tyy - Tyy0
+Tyz = Tyz - Tyz0
+Tzz = Tzz - Tzz0
+gN, gE, gD, TNN, TNE, TND, TEE, TED, TDD = \
+    rotate_vec_ten_ecef2ned(calc_lon_rad, calc_lat_rad,
+                            gx, gy, gz,
+                            Txx, Txy, Txz,
+                            Tyy, Tyz, Tzz)
+print(f'gD (mGal) --> min: {gD.min():.3e}; max: {gD.max():.3e}; mean: {gD.mean():.3e}; std: {gD.std():.3e}')
 
-# t0 = time.time()
-# V_g, gx_g, gy_g, gz_g, Txx_g, Txy_g, Txz_g, Tyy_g, Tyz_g, Tzz_g = \
-#     WerSch_numba(P, Verts, Faces, rho)
-# t_geo = time.time() - t0
-# time_geo.append(t_geo        
-# # Transform numerical results to NED
-# gN_geo, gE_geo, gD_geo, TNN_geo, TNE_geo, TND_geo, TEE_geo, TED_geo, TDD_geo = \
-#     rotate_vec_ten_ecef2ned(
-#         lon_rad, lat_rad,
-#         gx_g, gy_g, gz_g,
-#         Txx_g, Txy_g, Txz_g,
-#         Tyy_g, Tyz_g, Tzz_g
+gD_topoG_moon = pysh.SHGrid.from_array(gD.reshape(LON_P.shape))
+# %% 
+# # ! Topographic potential
+fig = pygmt.Figure()
+gD_topoG_moon.plotgmt(fig=fig,
+                      projection='mollweide',
+                      central_longitude=-90.,
+                      grid=[30, 30],
+                      tick_interval=None,
+                      cmap='haxby',
+                      cmap_limits=[-800, 800],
+                      colorbar='bottom',
+                      cb_triangles='both',
+                      cb_label='Downward gravity component (mGal)',
+                      axes_labelsize=12,
+                      cb_tick_interval=200,
+                      cb_minor_tick_interval=100)
+fig.show(width=800)
             
-# cal_geo = [V_g, gN_geo, gE_geo, gD_geo,
-#             TNN_geo, TNE_geo, TND_geo, TEE_geo, TED_geo, TDD_geo]
