de28c721b2498cd8027087f24e40ba20657e73f5
LeyuanWu <leyuanwu@zjut.edu.cn>
2026-05-09 17:18:58 +0800

    LW_20260509

--- ex_Moon_TopoGrav_Comp_SH.py
+++ ex_Moon_TopoGrav_Comp_SH.py
@@ -1,9 +1,19 @@
+##################################################################
+# # ! Moon Topographic-Potential Computation: Spherical Harmonics
+##################################################################
 # %% 
 # # ! Setup
 import numpy as np
 import pyshtools as pysh
 import pygmt
+import xarray as xr
 import time
+from datetime import datetime
+# %% 
+# # ! Start time
+print("=" * 80)
+print(f"Start time: [{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]")
+print("=" * 80)
 # %% 
 # # ! Constants
 myG = 6.67430e-11
@@ -14,7 +24,7 @@ r_calc = 1748000.0
 # # ! Shape of Moon
 #### * Shape
 lmax_shp = 359
-clm_shp_moon = pysh.SHCoeffs.from_file(f'Moon_shape_719.sh', 
+clm_shp_moon = pysh.SHCoeffs.from_file(f'input/Moon_shape_719.sh', 
                                        lmax=lmax_shp, 
                                        name='LOLA_shape (Moon)',
                                        units='m', format='bshc')
@@ -22,12 +32,14 @@ r_itfc = clm_shp_moon.coeffs[0,0,0]
 # %% 
 # # ! Computation of topographic potential: Spectral-domain 
 rho0 = 2560.0 # kg/m^3
-nmaxs = np.arange(1, 7)
+max_nmax = 6
+NMAXs = np.arange(1, max_nmax+1)
 clms = []
-for nmax in nmaxs:
+print('*'*32+'\n' + 'Computation of Topographic Potential: Spectral-domain\n' + '*'*32)
+for nmax in NMAXs:
     t0 = time.time()
     lmax = (lmax_shp+1)*nmax-1
-    clm_topoG_moon = \
+    clm_tgp_moon = \
         pysh.SHGravCoeffs.from_shape(shape=clm_shp_moon,
                                      rho=rho0,
                                      gm=gm_moon,
@@ -37,13 +49,15 @@ for nmax in nmaxs:
                                      lmax_calc=lmax_shp,
                                      name=f'LOLA_Topo_Grav (Moon) nmax={nmax}')
     tc = time.time() - t0
-    clms.append(clm_topoG_moon)
-    print(f"nmax = {nmax:2d}; lmax = {lmax:2d}; time cost: {tc:8.3f}")
+    clms.append(clm_tgp_moon)
+    print(f"nmax = {nmax:2d}; lmax = {lmax:4d}; time cost: {tc:8.2f} sec")
 # %% 
 # # ! Single contribution of each nmax in a band range
-print("nmax | l-beg | l-end | t-expand |  min_gz  |  max_gz  |  mean_gz |  std_gz")
+print('*'*32+'\n' + 'Single Contribution of Each nmax in a Band Range\n' + '*'*32)
+print(f"nmax | l-beg | l-end | t-expand |"
+      f"       min_gz |       max_gz |       mean_gz|       std_gz |")
 for i_clm in np.arange(len(clms)):
-    nmax = nmaxs[i_clm]
+    nmax = NMAXs[i_clm]
     if i_clm == 0:
         dif_clm = clms[i_clm]
     else:
@@ -68,13 +82,12 @@ for i_clm in np.arange(len(clms)):
         max_gz  = np.max(gz.data)
         mean_gz = np.mean(gz.data)
         std_gz  = np.std(gz.data)
-        fmt = ("{:4d} | {:5d} | {:5d} | {:8.4f} | "
-               "{:8.3f} | {:8.3f} | {:8.3f} | {:8.3f}")
+        fmt = ("{:4d} | {:5d} | {:5d} | {:8.2f} | "
+               "{:12.2e} | {:12.2e} | {:12.2e} | {:12.2e} |")
         print(fmt.format(nmax, lmax1+1, lmax2, t1,
                          min_gz, max_gz, mean_gz, std_gz))
 # %% 
 # # ! Topography
-#### * 
 grd_shp_moon = clm_shp_moon.expand()
 grd_topo_moon = (grd_shp_moon - r_itfc) / 1.e3
 fig = pygmt.Figure()
@@ -87,33 +100,58 @@ grd_topo_moon.plotgmt(fig=fig,
                       cmap_limits=[-8, 8],
                       colorbar='bottom',
                       cb_triangles='both',
-                      cb_label='Topography (km)',
+                      cb_label="Topography (km)",
                       cb_tick_interval=1,
                       cb_minor_tick_interval=0.5,
-                      shading=grd_topo_moon)
+                      shading=False)
+# fig.savefig('Moon_Topo_GMT.png', dpi=400)
 fig.show(width=800)
-fig.savefig('Moon_Topo.png', dpi=300)
 # %% 
 # # ! Topographic potential
-clm_topoG_moon = clms[0]
-grd_topoG_moon = clm_topoG_moon.expand(a=r_calc, f=0.0)
-gx_topoG_moon = -1.e5 * grd_topoG_moon.theta
-gy_topoG_moon =  1.e5 * grd_topoG_moon.phi
-gz_topoG_moon = -1.e5 * grd_topoG_moon.rad
+clm_tgp_moon = clms[-1]
+grd_tgp_moon = clm_tgp_moon.expand(a=r_calc, f=0.0)
+gx_tgp_moon = -1.e5 * grd_tgp_moon.theta
+gy_tgp_moon =  1.e5 * grd_tgp_moon.phi
+gz_tgp_moon = -1.e5 * grd_tgp_moon.rad
+arr_gx, arr_gy, arr_gz = gx_tgp_moon.data, gy_tgp_moon.data, gz_tgp_moon.data
+vars = ['gx', 'gy', 'gz']
+for var, arr in zip(vars, [arr_gx, arr_gy, arr_gz]):
+    print(f"Topographic Gravitational Potential: {var}")
+    print(f" min: {np.min(arr):12.4f} mGal\n max: {np.max(arr):12.4f} mGal \n"
+          f"mean: {np.mean(arr):12.4f} mGal\n std: {np.std(arr):12.4f} mGal")
 fig = pygmt.Figure()
-gz_topoG_moon.plotgmt(fig=fig,
-                      projection='mollweide',
-                      central_longitude=-90.,
-                      grid=[30, 30],
-                      tick_interval=None,
-                      cmap='haxby',
-                      cmap_limits=[-800, 800],
-                      colorbar='bottom',
-                      cb_triangles='both',
-                      cb_label='Downward gravity component (mGal)',
-                      axes_labelsize=12,
-                      cb_tick_interval=200,
-                      cb_minor_tick_interval=100,
-                      shading=grd_topo_moon)
+gz_tgp_moon.plotgmt(fig=fig,
+                    projection='mollweide',
+                    central_longitude=-90.,
+                    grid=[30, 30],
+                    tick_interval=None,
+                    cmap='haxby',
+                    cmap_limits=[-800, 800],
+                    colorbar='bottom',
+                    cb_triangles='both',
+                    cb_label='@[ g_z @[ (mGal)',
+                    axes_labelsize=12,
+                    cb_tick_interval=200,
+                    cb_minor_tick_interval=100,
+                    shading=False)
+# fig.savefig('Moon_TopoGz_GMT.png', dpi=400)
 fig.show(width=800)
-fig.savefig('Moon_Topo_Gz.png', dpi=300)
+# %% 
+# # ! Save gx, gy, gz to NetCDF
+nc_file = f'moon_topo_gravity_Lshp{lmax_shp}_nmax{max_nmax}.nc'
+lat = gz_tgp_moon.lats()
+lon = gz_tgp_moon.lons()
+res = int(180 * 60 / (len(lat) - 1))
+xr.Dataset(
+    {'gx': (('latitude', 'longitude'), arr_gx),
+     'gy': (('latitude', 'longitude'), arr_gy),
+     'gz': (('latitude', 'longitude'), arr_gz)},
+    coords={'longitude': lon, 'latitude': lat},
+    attrs={'units': 'mGal', 'reference_radius_m': r_calc, 'density_kg_m3': rho0}
+).to_netcdf(nc_file)
+print(f"Saved to '{nc_file}'")  
+# %% 
+# # ! End time
+print("=" * 80)
+print(f"End time: [{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]")
+print("=" * 80)
